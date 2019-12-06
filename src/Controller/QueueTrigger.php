cdde79b062941b11a9906352684b7aaea6cf6e4a
Steve Wirt <swirtMiles@138230.no-reply.drupal.org>
2019-12-06 00:49:34 -0500

    Issue #3095847: Add Queue trigger endpoint

---
+++ src/Controller/QueueTrigger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\govdelivery_bulletins\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * A controller for the GovDelivery Queue Trigger.
+ */
+class QueueTrigger extends ControllerBase {
+
+  /**
+   * Initiates the queue worker to process the queue.
+   */
+  public function processToEndTime() {
+    // Check to see if the config says the endpoint can be called.
+    $can_be_called = \Drupal::config('govdelivery_bulletins.settings')->get('api_queue_trigger_enabled');
+    $time = \Drupal::request()->query->get('EndTime');
+    // Check to see that $time is a valid timestamp.
+    if (!$can_be_called || empty($time) || (!is_numeric($time)) || ((string) (int) $time !== $time) || (strtotime(date('d-m-Y H:i:s', $time)) !== (int) $time)) {
+      // Either the endpoint can't be called, or the timestamp  was invalid.
+      // Return 404.
+      throw new NotFoundHttpException();
+    }
+    else {
+      // The endpoint has been called successfully.
+      // @Todo Call the queue runner service.
+      // Log it.
+      $vars = ['@time' =>  date('m-d-Y H:i:s', $time)];
+      $build = [
+        '#markup' => $this->t('The queue has been triggered for anything prior to: @time', $vars),
+      ];
+      \Drupal::logger('govdelivery_bulletins')->info('The queue has been triggered for anything prior to: @time', $vars);
+
+      return $build;
+    }
+  }
+}
