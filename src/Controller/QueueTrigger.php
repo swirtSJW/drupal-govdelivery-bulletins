e9f83240334f0fdb7cb0ae856d1a54ad5fba570b
Elijah Lynn <elijah@elijahlynn.net>
2021-07-15 14:30:29 -0700

    #3223883: Add query param EndTime to cache context for route controller.

--- src/Controller/QueueTrigger.php
+++ src/Controller/QueueTrigger.php
@@ -3,6 +3,7 @@
 namespace Drupal\govdelivery_bulletins\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Drupal\govdelivery_bulletins\Plugin\QueueWorker\BulletinBase;
 
@@ -14,10 +15,11 @@ class QueueTrigger extends ControllerBase {
   /**
    * Initiates the queue worker to process the queue.
    */
-  public function processToEndTime() {
+  public function processToEndTime(Request $request) {
     // Check to see if the config says the endpoint can be called.
     $can_be_called = \Drupal::config('govdelivery_bulletins.settings')->get('api_queue_trigger_enabled');
-    $time = \Drupal::request()->query->get('EndTime');
+    $time = $request->query->get('EndTime');
+
     // Check to see that $time is a valid timestamp.
     if (!$can_be_called || empty($time) || (!is_numeric($time)) || ((string) (int) $time !== $time) || (strtotime(date('d-m-Y H:i:s', $time)) !== (int) $time)) {
       // Either the endpoint can't be called, or the timestamp  was invalid.
@@ -33,6 +35,9 @@ class QueueTrigger extends ControllerBase {
       // Log status.
       $vars = ['@time' => date('m-d-Y H:i:s', $time)];
       $build = [
+        '#cache' => [
+          'contexts' => ['url.query_args:EndTime'],
+        ],
         '#markup' => $this->t('The queue has been triggered for anything prior to: @time', $vars),
       ];
       \Drupal::logger('govdelivery_bulletins')->info('The queue has been triggered for anything prior to: @time', $vars);
@@ -42,3 +47,4 @@ class QueueTrigger extends ControllerBase {
   }
 
 }
+
